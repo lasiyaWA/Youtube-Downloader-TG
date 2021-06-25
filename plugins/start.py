03d56c96d5e30438a869b3a3f0077bdfcd2c3092
darkhacker2005 <85599878+darkhacker2005@users.noreply.github.com>
2021-06-25 21:53:19 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,11 +6,10 @@ async def start(client, message):
     # return
     Lasiya = InlineKeyboardMarkup([
         
-        [InlineKeyboardButton("Youtube ❤", url="https://www.youtube.com/channel/UCHyseVcfusXkOClpwja00yg")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/Danuma_admin_bot")],
+            "Report Bugs 😊", url="https://t.me/joinchat/4WcL94IzXzswZGNl")],
         [InlineKeyboardButton(
-            "Bot channel 🧪",url="https://t.me/danumabots")]
+            "Bot channel 🧪",url="https://t.me/joinchat/4WcL94IzXzswZGNl")]
     ])
     thumbnail_url = "https://telegra.ph/file/69a96df53932f1cd2174f.jpg"
     await message.reply_photo(thumbnail_url, caption=f"Hi<b>{message.from_user.first_name}</b>\n\n<b>Instructions for use..</b>\n• Type /help to get instructins.\n• Type /tute for make a bot like me.\n───── ❝ **Lets Play** ❞ ─────\n ", reply_markup=Lasiya)
